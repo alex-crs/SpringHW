9e78c20afcc3d7dfc85769c0a310f643692b73b9
Andreev Aleksey <alexav5@yandex.ru>
2021-08-21 18:38:39 +0700

    Add homework1

---
+++ src/main/java/WebLayer/RandomProductResponse.java
@@ -0,0 +1,19 @@
+package WebLayer;
+
+import BLL.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Random;
+
+
+public class RandomProductResponse extends HttpServlet {
+
+}
